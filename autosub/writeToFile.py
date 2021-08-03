65f74c6f5529cef4b641a4c87d31c2cdd2f7f44f
Chan Qin Liang <65329090+qlchan24@users.noreply.github.com>
2021-08-03 17:47:06 +0800

    replace buggy try except clause

--- autosub/writeToFile.py
+++ autosub/writeToFile.py
@@ -4,6 +4,7 @@
 import os
 import datetime
 
+
 def write_to_file(file_handle, full_transcript_handle, inferred_text, line_count, limits, vtt, cues):
     """Write the inferred text to SRT file
     Follows a specific format for SRT files
@@ -15,29 +16,30 @@ def write_to_file(file_handle, full_transcript_handle, inferred_text, line_count
         limits : starting and ending times for text
     """
     full_transcript_handle.write(inferred_text + "\n")
-    
+
     sep = '.' if vtt else ','
-    
+
     d = str(datetime.timedelta(seconds=float(limits[0])))
-    try:
+    if "." in d:
         from_dur = "0" + str(d.split(".")[0]) + sep + str(d.split(".")[-1][:3])
-    except:
+    else:
         from_dur = "0" + str(d) + sep + "000"
-        
+
     d = str(datetime.timedelta(seconds=float(limits[1])))
     try:
         to_dur = "0" + str(d.split(".")[0]) + sep + str(d.split(".")[-1][:3])
     except:
         to_dur = "0" + str(d) + sep + "000"
 
-    if not vtt:    
+    if not vtt:
         file_handle.write(str(line_count) + "\n")
         file_handle.write(from_dur + " --> " + to_dur + "\n")
         file_handle.write(inferred_text + "\n\n")
     else:
-        file_handle.write(from_dur + " --> " + to_dur + "  align:start position:0%\n")
+        file_handle.write(from_dur + " --> " + to_dur +
+                          "  align:start position:0%\n")
         file_handle.write(inferred_text + "\n\n")
-        
+
 #         words = [f"<c> {x}</c>" for x in inferred_text.split(" ")]
 #         cue_tags = [f"<{str(datetime.timedelta(seconds=cue))}>" for cue in cues]
 #         words_cues_mixed = [val for pair in zip(cue_tags, words) for val in pair][1:]
